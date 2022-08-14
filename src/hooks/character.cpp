b3dbf5a24af1e4340020652acaf333a1ddfa28f5
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-14 07:52:14 +0300

    Revert Everything

--- src/hooks/character.cpp
+++ src/hooks/character.cpp
@@ -15,13 +15,13 @@ namespace Hooks
 
 		_Update = ActorVtbl.write_vfunc(REL::Relocate(0xAD, 0x0AD, 0x0AF), Update);
 
-		//_GetRunSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EB, 0x0EB, 0x0ED), GetRunSpeed);
-		//_GetJogSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EC, 0x0EC, 0x0EE), GetJogSpeed);
-		//_GetFastWalkSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0ED, 0x0ED, 0x0EF), GetFastWalkSpeed);
-		//_GetWalkSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EE, 0x0EE, 0x0F0), GetWalkSpeed);
+		_GetRunSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EB, 0x0EB, 0x0ED), GetRunSpeed);
+		_GetJogSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EC, 0x0EC, 0x0EE), GetJogSpeed);
+		_GetFastWalkSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0ED, 0x0ED, 0x0EF), GetFastWalkSpeed);
+		_GetWalkSpeed = ActorVtbl.write_vfunc(REL::Relocate(0x0EE, 0x0EE, 0x0F0), GetWalkSpeed);
 
-		////_ProcessTracking = ActorVtbl.write_vfunc(REL::Relocate(0x122, 0x122, 0x124), ProcessTracking);
-		//logger::info("  - ProcessTracking Address: {:X}", _ProcessTracking.address());
+		_ProcessTracking = ActorVtbl.write_vfunc(REL::Relocate(0x122, 0x122, 0x124), ProcessTracking);
+		logger::info("  - ProcessTracking Address: {:X}", _ProcessTracking.address());
 	}
 
 	// NPC Characters don't call UpdateAnimation like the PlayerCharacter
@@ -40,7 +40,7 @@ namespace Hooks
 					if ((anim_speed > 0.0) && (fabs(anim_speed - 1.0) > 1e-5)) {
 						float adjustment = current_delta * (anim_speed - 1.0);
 						if (current_delta + adjustment > 1e-5) {
-							//a_this->UpdateAnimation(adjustment);
+							a_this->UpdateAnimation(adjustment);
 						}
 					}
 				}
