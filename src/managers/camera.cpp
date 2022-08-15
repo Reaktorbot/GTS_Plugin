7a7620b424054c7a0d1891462d3e76b59e65b232
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-15 18:21:11 +0300

    Trying stuff

--- src/managers/camera.cpp
+++ src/managers/camera.cpp
@@ -46,7 +46,7 @@ namespace Gts {
 		}
 	}
 
-	float CameraManager::GetfOverShoulderPosZ() {
+	inline float CameraManager::GetfOverShoulderPosZ() {
 		auto camera = PlayerCamera::GetSingleton();
 		if (camera) {
 			TESCameraState* camera_state = camera->cameraStates[CameraStates::kThirdPerson].get();
@@ -57,7 +57,7 @@ namespace Gts {
 		}
 	}
 
-	float CameraManager::GetfOverShoulderPosX() {
+	inline float CameraManager::GetfOverShoulderPosX() {
 		auto camera = PlayerCamera::GetSingleton();
 		if (camera) {
 			TESCameraState* camera_state = camera->cameraStates[CameraStates::kThirdPerson].get();
@@ -68,7 +68,7 @@ namespace Gts {
 		}
 	}
 
-	float CameraManager::GetfOverShoulderPosY() {
+	inline float CameraManager::GetfOverShoulderPosY() {
 		auto camera = PlayerCamera::GetSingleton();
 		if (camera) {
 			TESCameraState* camera_state = camera->cameraStates[CameraStates::kThirdPerson].get();
