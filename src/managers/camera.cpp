cfc7a37c7bc7a6e7b6fab8d26cb9f8c0c7eab84f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-16 05:20:01 +0300

    setting.data.f->value

--- src/managers/camera.cpp
+++ src/managers/camera.cpp
@@ -14,7 +14,7 @@ namespace {
 		auto ini_conf = INIPrefSettingCollection::GetSingleton();
 		Setting* setting = ini_conf->GetSetting(name);
 		if (setting) {
-			setting->data->f = value; // If float
+			setting.data.f->value; // If float
 			ini_conf->WriteSetting(setting);
 		}
 	}
