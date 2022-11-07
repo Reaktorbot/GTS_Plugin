269a61dd574e20445b4a1e58c2a64e3f13295548
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-11-07 03:52:28 +0300

    Build Cosave

--- src/data/serde.hpp
+++ src/data/serde.hpp
@@ -2,11 +2,11 @@
 // Serde generalisation
 //
 
-namespace SKSE;
+using namespace SKSE;
 
 namespace Gts {
 	class Serde {
 		virtual void ser(SerializationInterface* serde, int version);
 		virtual void de(SerializationInterface* serde, int version);
-	}
+	} 
 }
